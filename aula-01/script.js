de0e3e6b28bb52a4ea0aade361c9e02e78181951
nicanico <nicsousan@gmail.com>
2023-02-09 16:50:59 -0300

    update calculadora de média - 09/02

--- aula-01/script.js
+++ aula-01/script.js
@@ -8,35 +8,35 @@ const buttonCalcular = document.getElementById('calcular')
 const resultadoInput = document.getElementById('resultado')
 const formulario = document.getElementById('formulario')
 const aprovado = 'Parabens! O aluno foi aprovado'
-const reprovado = 'Aluno reprovado' 
+const reprovado = 'Aluno reprovado'
 
 
 
-function media (){
+function media() {
     situacao.value = (Number(nota1.value) + Number(nota2.value)) / 2
 
-    if(nota1.value > 10  || nota1.value < 0 || nota2.value > 10 || nota2.value < 0){
-    formulario.classList.add('sombra-amarela')
-    formulario.classList.remove('sombra-verde')
-    formulario.classList.remove('sombra-vermelha')
-    situacao.value = 'Insira números de 0 a 10'
-}else{
-    if(situacao.value > 5){
-        situacao.value = "aprovado"
-        formulario.classList.add('sombra-verde')
-        formulario.classList.remove('sombra-amarela')
+    if (nota1.value > 10 || nota1.value < 0 || nota2.value > 10 || nota2.value < 0) {
+        formulario.classList.add('sombra-amarela')
+        formulario.classList.remove('sombra-verde')
         formulario.classList.remove('sombra-vermelha')
-    }else if(situacao.value < 5){
-        situacao.value = "reprovado"
-        formulario.classList.add('sombra-vermelha')
-        formulario.classList.remove('sombra-amarela')
-        formulario.classList.remove('sombra-vermelha')
+        situacao.value = 'Insira números de 0 a 10'
+    } else {
+        if (situacao.value > 5) {
+            situacao.value = "aprovado"
+            formulario.classList.add('sombra-verde')
+            formulario.classList.remove('sombra-amarela')
+            formulario.classList.remove('sombra-vermelha')
+        } else if (situacao.value < 5) {
+            situacao.value = "reprovado"
+            formulario.classList.add('sombra-vermelha')
+            formulario.classList.remove('sombra-amarela')
+            formulario.classList.remove('sombra-vermelha')
 
 
-        
 
+
+        }
     }
-}
 }
 
 buttonCalcular.addEventListener('click', media)
