00d99fdd5b7d0e83aa1887ac4ffeb08cebcff511
nicanico <nicsousan@gmail.com>
2023-02-16 20:38:25 -0300

    update 16/02 - resolução e implementação de tratamento

--- aula-02/main.js
+++ aula-02/main.js
@@ -7,23 +7,23 @@ const adicionar = document.getElementById('adicionar')
 //     container.innerHTML = container.innerHTML + '<div class="item"></div>'
 // }
 
-const adicionarCard = (nome, nota) => {
+const adicionarCard = (nomeAluno, notaAluno) => {
     const container = document.getElementById('container')
     const newDiv = document.createElement('div') /* novo objeto*/
     newDiv.classList.add('aluno') /* adiciona uma classe*/
-    newDiv.innerHTML = `<h2 class="aluno_nome">${nome}</h2>
-                        <span class="aluno_nota">${nota}</span>`
+    newDiv.innerHTML = `<h2 class="aluno_nome">${nomeAluno}</h2>
+                        <span class="aluno_nota">${notaAluno}</span>`
     /* podemos usar o innerHTML antes da linha de anexo*/
-    if(valor == '' || nome == ''){
-        alert('Você deixou campos vazios')
-    }else{ 
-        if(valor < 5){
-        newDiv.classList.add('fundo-vermelha')
-        newDiv.classList.remove('fundo-verde')
-        }else if(valor > 5){
 
-        newDiv.classList.add('fundo-verde')
-        newDiv.classList.remove('fundo-vermelha')
+    if (notaAluno == '' || nomeAluno == '') {
+        alert('Você deixou campos vazios')
+    } else {
+        if (notaAluno < 5) {
+            newDiv.classList.add('fundo-vermelha')
+            newDiv.classList.remove('fundo-verde')
+        } else if (notaAluno > 5) {
+            newDiv.classList.add('fundo-verde')
+            newDiv.classList.remove('fundo-vermelha')
         }
     }
     container.appendChild(newDiv)/* anexa sem ler novamente*/
@@ -32,11 +32,17 @@ const adicionarCard = (nome, nota) => {
 /* essa função serve exclusivamente para chamar o adicionarCard */
 const hadleClick = () => {
     /* caixa de texto para digitação */
-    const nome = prompt('Digite o nome do aluno')
-    const nota = prompt('Digite a nota do aluno')
-    let valor = Number(nota)
-    
-adicionarCard(nome, nota)
+    const aluno = {
+        nome: prompt('Digite o nome do aluno'),
+        nota: prompt('Digite a nota do aluno')
+    }
+    const nomeAluno = aluno.nome
+    if (isNaN(aluno.nota)) {
+        alert('A nota passada precisa ser numerica')
+    } else {
+        const notaAluno = Number(aluno.nota)
+        adicionarCard(nomeAluno, notaAluno)
+    }
 }
 
 adicionar.addEventListener('click', hadleClick)
